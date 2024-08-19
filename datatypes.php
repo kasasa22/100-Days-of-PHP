01de7d4f91744aca5253169022371852166c32bf
Trevor22 <kasasatrevor25@gmail.com>
2024-08-19 12:31:58 +0300

    data-types

---
+++ datatypes.php
@@ -0,0 +1,30 @@
+<?php
+//DATA TYPES
+
+//STRINGS
+$string = "Hello World";
+$num= "2";
+
+//INTEGERS
+$int = 10;
+$int2 = 20;
+
+//FLOATS
+$float = 10.5;
+$float2 = 20.5;
+
+//BOOLEAN
+$bool = true;
+$bool2 = false;
+//bolleans are used for conditional statements
+
+//ARRAYS
+$numbers = [1,2,3,4,5,6,7,8,9,10];
+$names = ["John","Jane","Joe","Jill"];
+
+//NULL
+$null = null;
+
+//OBJECT    
+
+?>
